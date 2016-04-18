2410dabe99b8182b59724526c6a63d5ab8d26e7d
Brian Capouch <brianc@palaver.net>
2016-04-18 14:50:40 -0400

    Intial Commit

---
+++ AnimalsBLC/src/edu/saintjoe/cs/brianc/animals/Animal.java
@@ -0,0 +1,18 @@
+package edu.saintjoe.cs.brianc.animals;
+
+public class Animal {
+	
+	String picture;
+	String location;
+	
+	/* ---- */
+	
+	public String makeNoise() {
+		return ("Ommmmmmmmmmmmmm");
+	}
+	
+	public void eat () {
+		System.out.println("Animal is eating");
+	}
+
+}
---
+++ AnimalsBLC/src/edu/saintjoe/cs/brianc/animals/Lion.java
@@ -0,0 +1,5 @@
+package edu.saintjoe.cs.brianc.animals;
+
+public class Lion extends Cat {
+
+}
